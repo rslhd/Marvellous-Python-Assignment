015974077212fe0dd8006b6573f141548f70b8ce
rslhd <165342853+rslhd@users.noreply.github.com>
2025-05-23 20:54:56 +0530

    Create Assignment7_1.py

---
+++ Assignment7/Assignment7_1.py
@@ -0,0 +1,10 @@
+Square = lambda a : a * a
+Cube = lambda a : a**3
+
+ret1 = int(input("Enter a number"))
+
+Sq = Square(ret1)
+Cu = Cube(ret1)
+
+print("Square of number is ", Sq)
+print("Cube of number is ", Cu)
