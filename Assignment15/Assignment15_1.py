e8f7848d35d936d8607bda1e90d40369bd2e82dd
rslhd <165342853+rslhd@users.noreply.github.com>
2025-06-16 22:12:49 +0530

    Create Assignment15_1.py

---
+++ Assignment15/Assignment15_1.py
@@ -0,0 +1,15 @@
+import os 
+
+def main():
+    fileName = input("Enter the file name to check  ")
+
+    ret = os.path.exists(fileName)
+
+    if ret == True :
+        print ("File exists")
+    
+    else :
+        print("File does not exist") 
+
+if __name__ == "__main__":
+    main()
