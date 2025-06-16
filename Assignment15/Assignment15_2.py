1a37d261c7974b3e6a85f4430c5ac700cc5e8c81
rslhd <165342853+rslhd@users.noreply.github.com>
2025-06-16 22:14:40 +0530

    Add files via upload

---
+++ Assignment15/Assignment15_2.py
@@ -0,0 +1,22 @@
+import os 
+
+def main():
+    fileName = input("Enter the file name to check : ")
+
+    ret = os.path.exists(fileName)
+
+    if ret == True :
+        print ("File exists")
+        fobj = open(fileName,"r")
+        
+        data = fobj.read()
+
+        print("Data from file is : ",data)
+
+        fobj.close()
+            
+    else :
+        print("File does not exist") 
+
+if __name__ == "__main__":
+    main()
